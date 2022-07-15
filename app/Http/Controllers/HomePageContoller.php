19967d03cf2047cae9ff860f65af6b7476ee604a
Tural Samadli <tsamadli4696@ada.edu.az>
2022-07-15 19:39:35 +0400

    first commit

---
+++ app/Http/Controllers/HomePageContoller.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\Models\about;
+use app\Models\blog;
+use app\Models\contact;
+use app\Models\portfolio;
+use app\Models\resume;
+use app\Models\services;
+use app\Models\user_messages;
+use App\Http\Requests\UserMessages;
+
+
+
+
+
+
+
+
+class HomePageContoller extends Controller
+{
+    public function index(){
+        $contact=contact::where('id',1)->first();
+        $about=about::where('id',1)->first();
+        return view('homepage', compact('contact','about'));
+
+    }
+    // public function user_messages(UserMessages $request)
+    // {
+    //     $send = UserMessages::create([
+    //         'name'      => $request->name,
+    //         'email'     => $request->email,
+    //         'subject'   => $request->subject,
+    //         'message'   => $request->message
+            
+    //     ]);
+    //     if ($send)
+    //     {
+    //         return redirect()->route('home');
+    //     }
+       
+    // }
+
+}
